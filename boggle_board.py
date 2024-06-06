5950c5f7372e63973e4fabe1a5a604346cbbf8c1
Crusader-RG <justin.leeper92@gmail.com>
2024-06-06 12:09:44 -0600

    initial gen_2 complete

--- boggle_board.py
+++ boggle_board.py
@@ -53,7 +53,53 @@ class BoggleBoard_gen1:
     
 
 
-game1 = BoggleBoard_gen1("game1")
-game1.shake()
-BoggleBoard_gen1.shake()
+# game1 = BoggleBoard_gen1("game1")
+# game1.shake()
+# BoggleBoard_gen1.shake()
       
+class BoggleBoard_gen2(BoggleBoard_gen1):
+
+  ltr_dict = {
+    1: list("AAEEGN"),
+    2: list("ELRTTY"),
+    3: list("AOOTTW"),
+    4: list("ABBJOO"),
+    5: list("EHRTVW"),
+    6: list("CIMOTU"),
+    7: list("DISTTY"),
+    8: list("EIOSST"),
+    9: list("DELRVY"),
+    10: list("ACHOPS"),
+    11: list("HIMNQU"),
+    12: list("EEINSU"),
+    13: list("EEGHNW"),
+    14: list("AFFKPS"),
+    15: list("HLNNRZ"),
+    16: list("DEILRX")  
+    }
+
+
+  
+
+  def __init__(self, name):
+    super().__init__(name)
+    self.list = random.sample(range(1, 17), 16)
+    self.board = []
+
+
+  def shake(self):
+    tick = 0
+    for i in range(0,16,4):
+      num = random.randint(0,5)
+      self.board.append(self.ltr_dict[self.list[i]][num])
+      
+      for j in range(i+1,i+4):
+        num = random.randint(0,5)
+        self.board[tick] += f" {self.ltr_dict[self.list[j]][num]}"
+      tick += 1
+    for k in range(0,4):
+      print(self.board[k])
+
+
+game1 = BoggleBoard_gen2("game1")
+game1.shake()
